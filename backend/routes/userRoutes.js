6de88468974975731789dd8f4b65fa14dfa3985c
Juvens Helas <407juvens@gmail.com>
2025-05-30 18:38:43 -0700

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,5 +5,7 @@ const userController = require('../controllers/userController');
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.get('/dashboard/:userId', userController.getDashboard);
+router.get('/session', userController.getUserBySession);
+
 
 module.exports = router;
